55866ade946df71ca05049eff007b67cf913de6e
thinktwice13 <marioo.krajacic@gmail.com>
2022-06-29 15:02:21 +0200

    Fetch service options

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,52 @@
 import {Box, Heading, Stack} from "@chakra-ui/react";
-import type { NextPage } from 'next'
+import type {GetStaticProps, GetStaticPropsResult, InferGetStaticPropsType, NextPage} from 'next'
 import Service from "../lib/services/Service"
 
-const services = [
-  { name: 'service', id: '1' },
-  { name: 'service', id: '2' },
-  { name: 'service', id: '3' },
-  { name: 'service', id: '4' },
-  { name: 'service', id: '5' }
-]
-
-
-const Home: NextPage = () => {
+const Home: NextPage = (props: InferGetStaticPropsType<typeof getStaticProps>
+) => {
   return (
     <Box>
-    <Heading>CPV</Heading>
+      <Heading>CPV</Heading>
       <Stack>
-        {services?.map(service => <Service key={service.id} id={+service.id} name={service.name}/>)}
+        {props.services?.map(service => <Service key={service.id} id={+service.id} name={service.name}/>)}
       </Stack>
     </Box>
   )
+}
+
+interface ServiceData {
+  id: string
+  name: string
+}
+
+export const getStaticProps: GetStaticProps = async (): Promise<GetStaticPropsResult<{ services: ServiceData[] }>> => {
+  const twentyFourHoursInSecs = 24 * 60 * 60
+  try {
+    const services = await getServiceOptions()
+    if (services.length == 0) {
+      throw new Error('failed to fetch services')
+    }
+
+    return {revalidate: 3 * twentyFourHoursInSecs, props: {services}}
+  } catch (err) {
+    console.log(err)
+    return {revalidate: twentyFourHoursInSecs, props: {services: []}}
+  }
+}
+
+const servicesUrl = 'http://www.valencia.es/qsige.localizador/citaPrevia/servicios/disponibles/'
+
+function getServiceOptions(): Promise<ServiceData[]> {
+  return fetch(servicesUrl)
+    .then((resp) => {
+      if (!resp.ok) {
+        throw new Error(resp.status.toString())
+      }
+      return resp.json()
+    })
+    .then(res => {
+      return res.reduce((acc, svc) => [...acc, {id: svc.id_servicio, name: svc.nombre}], [])
+    })
 }
 
 export default Home
