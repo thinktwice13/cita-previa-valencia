22d3164e4c64fa03de3ca77d89945a4515810462
thinktwice13 <marioo.krajacic@gmail.com>
2022-06-29 14:34:12 +0200

    Initial setup, use ChakraUI, src dir, testing-library

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import {Box, Heading} from "@chakra-ui/react";
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <Box>
+    <Heading>CPV</Heading>
+    </Box>
+  )
+}
+
+export default Home
